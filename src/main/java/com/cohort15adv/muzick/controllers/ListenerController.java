822753dbd223c309ed7ef5c0eedefee6328c08b9
benrmartins <benrmartins@gmail.com>
2022-06-28 20:03:28 -0400

    practice

--- src/main/java/com/cohort15adv/muzick/controllers/ListenerController.java
+++ src/main/java/com/cohort15adv/muzick/controllers/ListenerController.java
@@ -1,12 +1,15 @@
 package com.cohort15adv.muzick.controllers;
 
 import com.cohort15adv.muzick.models.Listener;
-import com.cohort15adv.muzick.repositories.ListenerRespository;
+import com.cohort15adv.muzick.repositories.ListenerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
+import java.util.Optional;
 
 
 @CrossOrigin
@@ -15,29 +18,40 @@ import java.util.List;
 public class ListenerController {
 
     @Autowired
-    private ListenerRespository listenerRepository;
+    private ListenerRepository listenerRepository;
 
     @GetMapping("/test")
     public ResponseEntity<?> testRoute() {
         return new ResponseEntity<>("Hello World", HttpStatus.OK);
     }
-
-    // create, read, update, delete
+    // Create, Read, Update, Delete
+    // Create = POST
+    // Read = GET
+    // Update = PUT* POST
+    // Delete = DELETE
 
     @PostMapping("/")
-    public ResponseEntity<?> createListener(@RequestBody Listener newListener) {
-        Listener listener = listenerRepository.save();
+    public ResponseEntity<Listener> createListener(@RequestBody Listener newListener) {
+        Listener listener = listenerRepository.save(newListener);
+
         return new ResponseEntity<>(listener, HttpStatus.CREATED);
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> getAllListeners() {
-        List<Listener> listener = listenerRepository.findAll();
-        return new ResponseEntity<>(listener, HttpStatus.OK);
+    public ResponseEntity<List<Listener>> getAllListeners() {
+        List<Listener> listeners = listenerRepository.findAll();
+        return new ResponseEntity<>(listeners, HttpStatus.OK);
     }
 
-    @GetMapping("/{ListenerId}/{noteID}")
-    public ResponseEntity<?> getOneLinstener(@PathVariable, Long id) {
-        listenerRepository.findById(id).orElseThrow(new Repon);
+    @GetMapping("/{id}")
+    public ResponseEntity<Listener> getOneListener(@PathVariable Long id) {
+//        Listener listener = listenerRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Optional<Listener> maybeListener = listenerRepository.findById(id);
+
+        if (maybeListener.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(maybeListener.get(), HttpStatus.OK);
     }
 }
--- src/main/java/com/cohort15adv/muzick/models/Listener.java
+++ src/main/java/com/cohort15adv/muzick/models/Listener.java
@@ -1,17 +1,22 @@
 package com.cohort15adv.muzick.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Listener {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // UUID
     private Long id;
     private String name;
     private Integer age;
+    // later we will add genre and user
+
+    @OneToMany(mappedBy = "listener", fetch = FetchType.LAZY) // match the variable name in the Entity class
+    @JsonIgnore
+    private Set<Note> notes;
 
     public Listener() {
     }
@@ -22,24 +27,32 @@ public class Listener {
         this.age = age;
     }
 
+    public Set<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Set<Note> notes) {
+        this.notes = notes;
+    }
+
     public Long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getAge() {
-        return age;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 
     public void setAge(Integer age) {
